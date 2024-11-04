a622752c457cb7580b8ce4b15986a3c32567a20d
tino <mocayaragustin18@gmail.com>
2024-11-04 19:46:56 -0300

    l

--- app.py
+++ app.py
@@ -78,5 +78,6 @@ def consultar_pronostico():
         return render_template('error.html', error='Compruebe el nombre de la ciudad e intente de nuevo')
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
     # app.run(debug=True)
     app.run(host='0.0.0.0', port=5000)
